65740b88ef87b6dd06a26465f019510b079748b6
Shlomi Levi <wizardnet972@gmail.com>
2025-04-07 15:34:44 +0300

    Enhance ESLint configuration for JSON files, update nx.json with new build dependencies, add jsonc-eslint-parser to package.json, and adjust TypeScript paths in tsconfig.base.json.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
 import nx from '@nx/eslint-plugin';
 
 export default [
+  {
+    files: ['**/*.json'],
+    // Override or add rules here
+    rules: {},
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
---
+++ packages/nx-domainify/README.md
@@ -0,0 +1,11 @@
+# nx-domainify
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-domainify` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-domainify` to execute the unit tests via [Vitest](https://vitest.dev/).
